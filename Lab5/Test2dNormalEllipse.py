4d94bf5da35d8f916b29d94a537e35291a14bbb4
Grigory P <48101281+via8@users.noreply.github.com>
2020-04-27 20:19:52 +0300

    Fix Lab5 ellipse test (2)

--- Lab5/Test2dNormalEllipse.py
+++ Lab5/Test2dNormalEllipse.py
@@ -16,23 +16,19 @@ def plot_dispersion_ellipse(samples, corr_coeff, subplot):
     angle_rad = numpy.pi / 4
     if abs(dispersion_x - dispersion_y) > 1e-3:
         angle_rad = numpy.arctan(2.0 * corr_coeff * sigma_x * sigma_y / (dispersion_x - dispersion_y)) / 2.0
-    # mean_x = 0
-    # mean_y = 0
-    # dispersion_x = 1
-    # dispersion_y = 1
-    # sigma_x = 1
-    # sigma_y = 1
-    # angle_rad = numpy.pi / 4.0
+    if corr_coeff == 0:
+        angle_rad = 0
+
     cos = numpy.cos(angle_rad)
     sin = numpy.sin(angle_rad)
-    A = 1 / (dispersion_x * (1 - corr_coeff * corr_coeff))
-    B = 1 / (dispersion_y * (1 - corr_coeff * corr_coeff))
-    C = corr_coeff / (sigma_x * sigma_y * (1 - corr_coeff * corr_coeff))
-    a = 2 / numpy.sqrt(A * cos * cos + B * sin * sin - 2 * C * sin * cos)
-    b = 2 / numpy.sqrt(A * cos * cos + B * sin * sin + 2 * C * sin * cos)
-    ellipse = patches.Ellipse((mean_x, mean_y), a * 2, b * 2, numpy.rad2deg(angle_rad))
+
+    a = numpy.sqrt(dispersion_x * cos * cos + 2 * corr_coeff * sigma_x * sigma_y * sin * cos + dispersion_y * sin * sin)
+    b = numpy.sqrt(dispersion_x * sin * sin - 2 * corr_coeff * sigma_x * sigma_y * sin * cos + dispersion_y * cos * cos)
+
+    ellipse = patches.Ellipse((mean_x, mean_y), width=a * 4.9, height=b * 4.9, angle=numpy.rad2deg(angle_rad))
+
     subplot.set_title("$\\rho$ = " + str(corr_coeff))
-    subplot.plot(samples[0], samples[1], 'ro', markersize=2)
+    subplot.plot(samples[0], samples[1], 'ro', markersize=3)
     subplot.add_patch(ellipse)
     subplot.axis('scaled')
 
