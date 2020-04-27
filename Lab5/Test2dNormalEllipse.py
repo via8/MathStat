849e795feea5c6a1c6420441625f925b8f62ff29
Grigory P <48101281+via8@users.noreply.github.com>
2020-04-27 16:07:28 +0300

    Fix Lab5 ellipse test

--- Lab5/Test2dNormalEllipse.py
+++ Lab5/Test2dNormalEllipse.py
@@ -7,22 +7,34 @@ import StatisticalCharacteristics as statchars
 
 def plot_dispersion_ellipse(samples, corr_coeff, subplot):
     assert (len(samples) == 2)
+    mean_x = statchars.mean(samples[0])
+    mean_y = statchars.mean(samples[1])
     dispersion_x = statchars.dispersion(samples[0])
     dispersion_y = statchars.dispersion(samples[1])
     sigma_x = numpy.sqrt(dispersion_x)
     sigma_y = numpy.sqrt(dispersion_y)
-    angle_rad = numpy.arctan(2.0 * corr_coeff * sigma_x * sigma_y / (dispersion_x - dispersion_y)) / 2.0
+    angle_rad = numpy.pi / 4
+    if abs(dispersion_x - dispersion_y) > 1e-3:
+        angle_rad = numpy.arctan(2.0 * corr_coeff * sigma_x * sigma_y / (dispersion_x - dispersion_y)) / 2.0
+    # mean_x = 0
+    # mean_y = 0
+    # dispersion_x = 1
+    # dispersion_y = 1
+    # sigma_x = 1
+    # sigma_y = 1
+    # angle_rad = numpy.pi / 4.0
     cos = numpy.cos(angle_rad)
     sin = numpy.sin(angle_rad)
     A = 1 / (dispersion_x * (1 - corr_coeff * corr_coeff))
     B = 1 / (dispersion_y * (1 - corr_coeff * corr_coeff))
     C = corr_coeff / (sigma_x * sigma_y * (1 - corr_coeff * corr_coeff))
-    a = 1 / numpy.sqrt(A * cos * cos + B * sin * sin - 2 * C * sin * cos)
-    b = 1 / numpy.sqrt(A * cos * cos + B * sin * sin + 2 * C * sin * cos)
-    ellipse = patches.Ellipse((statchars.mean(samples[0]), statchars.mean(samples[1])), a * 4, b * 4, numpy.rad2deg(angle_rad))
+    a = 2 / numpy.sqrt(A * cos * cos + B * sin * sin - 2 * C * sin * cos)
+    b = 2 / numpy.sqrt(A * cos * cos + B * sin * sin + 2 * C * sin * cos)
+    ellipse = patches.Ellipse((mean_x, mean_y), a * 2, b * 2, numpy.rad2deg(angle_rad))
     subplot.set_title("$\\rho$ = " + str(corr_coeff))
-    subplot.plot(samples[0], samples[1], 'ro')
+    subplot.plot(samples[0], samples[1], 'ro', markersize=2)
     subplot.add_patch(ellipse)
+    subplot.axis('scaled')
 
 
 # parameters
